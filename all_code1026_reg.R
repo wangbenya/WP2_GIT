ca6e7d9b7328f21c92d001b53b3b45882294560d
wangbenya <benya.wang@hotmail.com>
2017-10-26 17:25:45 +0800

    NC

--- all_code1026_reg.R
+++ all_code1026_reg.R
@@ -600,15 +600,14 @@ for (tt in c(1:10)){
 
 dim(all_results)
 
-for (a1 in seq(0.5,1.5,0.1)){
-  for (a2 in seq(1.0,3.5,0.1)){
-    
+for (a1 in c(0.5,1.0,1.5)){
+  for (a2 in c(1.0,1.5,2.0,2.5,3.0,3.5)){
+    if (a2-a1>=0.5){
       p1<-reclass4(all_results[,c(3,2)],a1,a2)
       p2<-reclass4(all_results[,c(5,4)],a1,a2)
       p4<-reclass4(all_results[,c(7,6)],a1,a2)
       print(table(p1[,2]))
       sing_acc<-data.frame(p1=postResample(p1[,1],p1[,2])[1],p2=postResample(p2[,1],p2[,2])[1],p4=postResample(p4[,1],p4[,2])[1])
-      if ((a2-a1>=0.5)&(postResample(p4[,1],p4[,2])[1]-postResample(p2[,1],p2[,2])[1]>=0.3)){
       print(a1)
       print(a2)
       print(sing_acc)
