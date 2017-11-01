43df52d277906a2f757f7db58eada23f8f09b9f1
wangbenya <benya.wang@hotmail.com>
2017-11-01 05:55:38 +0000

    NC

--- all_code1029_cla.R
+++ all_code1029_cla.R
@@ -255,7 +255,7 @@ TN_GW4<-read.csv("~/WP2_GIT/TN_GW4.csv",header = T)
       para_rf2 = makeParamSet(
         makeDiscreteParam("ntree", values=seq(200,800,10)),
         makeIntegerParam("nodesize", lower = 3, upper = 8),
-        makeIntegerParam("mtry", lower = 4, upper = 12)
+        makeIntegerParam("mtry", lower = 4, upper =10 )
       )
 
       model_build <- function(dataset, n_target,nn) {
