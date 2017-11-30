f2067fc669b1bce965a6308b029553e1a7427a0a
wangbenya <benya.wang@hotmail.com>
2017-11-30 17:39:39 +0800

    NC

--- all_code1123_reg.R
+++ all_code1123_reg.R
@@ -207,8 +207,8 @@ newdata$DON_m5<-(newdata$DON.1+newdata$DON.2+newdata$DON.3+newdata$DON.4+newdata
 newdata$variance<-(newdata$DON-newdata$DON_m5)^2
 newdata$dev<-abs(newdata$DON-newdata$DON_m5)/newdata$DON_m5
 
-newdata[newdata$dev<=1.5,"type"]=1
-newdata[newdata$dev>1.5,"type"]=0
+newdata[newdata$dev<=1,"type"]=1
+newdata[newdata$dev>1,"type"]=0
 
 all_points<-data.frame(newdata)
 
@@ -225,7 +225,7 @@ rdesc = makeResampleDesc("CV", iters = 5)
 ## define the parameter spaces for RF      
 para_rf = makeParamSet(
   makeDiscreteParam("ntree", values=seq(50,200,20)),
-  makeIntegerParam("nodesize", lower = 10, upper = 15),
+  makeIntegerParam("nodesize", lower = 5, upper = 10),
   makeIntegerParam("mtry", lower = 2, upper =6)
 #  makeDiscreteParam("coefReg", values=seq(0.05,0.2,0.05))
 )
