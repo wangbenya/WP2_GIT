20202068c47ec444348c756bf2b70ed380fba9b4
wangbenya <benya.wang@hotmail.com>
2017-11-30 17:10:15 +0800

    NC

--- all_code1123_reg.R
+++ all_code1123_reg.R
@@ -207,8 +207,8 @@ newdata$DON_m5<-(newdata$DON.1+newdata$DON.2+newdata$DON.3+newdata$DON.4+newdata
 newdata$variance<-(newdata$DON-newdata$DON_m5)^2
 newdata$dev<-abs(newdata$DON-newdata$DON_m5)/newdata$DON_m5
 
-newdata[newdata$dev<=1.2,"type"]=1
-newdata[newdata$dev>1.2,"type"]=0
+newdata[newdata$dev<=1.5,"type"]=1
+newdata[newdata$dev>1.5,"type"]=0
 
 all_points<-data.frame(newdata)
 
