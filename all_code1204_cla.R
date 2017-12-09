740f8a46f6e89785978055abe5b026ca98470d93
wangbenya <benya.wang@hotmail.com>
2017-12-09 14:41:02 +0000

    NC

--- all_code1204_cla.R
+++ all_code1204_cla.R
@@ -269,7 +269,7 @@ for (tt in c(1:30)){
   seeds<-seed.list[tt]
   set.seed(seeds)
 
-  trainIndex <- createDataPartition(all_points$DON, p = 0.8, list = FALSE)  
+  trainIndex <- createDataPartition(all_points$DON, p = 0.75, list = FALSE)  
   training <- all_points[trainIndex,]
   testing <- all_points[-trainIndex,]
   
