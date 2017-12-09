58193472e3b5da39142cf398da60e4a75d15b05d
wangbenya <benya.wang@hotmail.com>
2017-12-09 21:45:38 +0800

    NC

--- all_code1204_cla.R
+++ all_code1204_cla.R
@@ -221,8 +221,8 @@ newdata$DON_m3<-(newdata$DON.1+newdata$DON.2+newdata$DON.3)/3
 
 newdata$dev<-abs(newdata$DON-newdata$DON_m3)/newdata$DON_m3
 
-newdata[newdata$dev<=6,"type"]=1
-newdata[newdata$dev>6,"type"]=0
+newdata[newdata$dev<=5,"type"]=1
+newdata[newdata$dev>5,"type"]=0
 
 all_points<-data.frame(newdata)
 all_points<-subset(all_points,all_points$type==1)
