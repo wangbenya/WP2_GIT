aac8b2a61e77549881ea983c7f819b4344f3fb32
wangbenya <wangbenya@users.noreply.github.com>
2018-01-02 14:58:04 +0800

    add the nodesize

--- all_code1204_cla.R
+++ all_code1204_cla.R
@@ -249,7 +249,7 @@ rdesc = makeResampleDesc("CV", iters = 5)
 ## define the parameter spaces for RF      
 para_rf = makeParamSet(
   makeDiscreteParam("ntree", values=seq(200,500,50)),
-  makeIntegerParam("nodesize", lower = 35, upper = 40),
+  makeIntegerParam("nodesize", lower = 20, upper = 25),
   makeIntegerParam("mtry", lower = 2, upper =3)
   #  makeDiscreteParam("coefReg", values=seq(0.05,0.2,0.05))
 )
@@ -482,3 +482,5 @@ for (tt in c(1:5)){
   print(all_results)
  
 }
+
+summary(all_results)
