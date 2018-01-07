fc7ec1844b5811dc85121a3cfcf2a67463771b82
wangbenya <wangbenya@users.noreply.github.com>
2018-01-07 13:12:16 +0800

    change nodesize and mtry

--- all_code1204_cla.R
+++ all_code1204_cla.R
@@ -251,8 +251,8 @@ rdesc = makeResampleDesc("CV", iters = 5)
 ## define the parameter spaces for RF      
 para_rf = makeParamSet(
   makeDiscreteParam("ntree", values=seq(50,500,50)),
-  makeIntegerParam("nodesize", lower = 20, upper = 30),
-  makeIntegerParam("mtry", lower = 4, upper =8)
+  makeIntegerParam("nodesize", lower = 60, upper = 65),
+  makeIntegerParam("mtry", lower = 2, upper =3)
   #  makeDiscreteParam("coefReg", values=seq(0.05,0.2,0.05))
 )
 
