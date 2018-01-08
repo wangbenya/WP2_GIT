db071b9b487b68256958f0c7488a6522552419de
wangbenya <benya.wang@hotmail.com>
2018-01-08 17:34:02 +0800

    tune the change

--- all_code1204_cla.R
+++ all_code1204_cla.R
@@ -205,6 +205,8 @@ names(landscapes) <- c("Soil", "Veg", "Landuse","Catchment", "GW_depth", "Distan
 
 ## load the data 
 set.seed(666)
+for(aaaa in seq(0.25,0.45,0.05)){
+
 seed.list<-sample(1:1000,300,replace =F)
 all_points<-read.csv("~/WP2/data/all_data1127.csv",header = T)
 extra_n<-read.csv("~/WP2/data/extra_n.csv",header = T)
@@ -236,7 +238,7 @@ newdata[newdata$dev>5,"type"]=0
 all_points<-data.frame(newdata)
 all_points<-subset(all_points,all_points$type==1)
 
-#all_points[all_points$DON==0.25,"DON"]=0.75
+all_points[all_points$DON==0.25,"DON"]=aaaa
 
 ## set the parameters for mlr
 seed=35
@@ -289,12 +291,12 @@ a1=1.0
 a2=2.0
 all_results<-data.frame()
 
-for (tt in c(1:20)){
-  print(tt+30)
+for (tt in c(1:10)){
+  print(tt)
   seeds<-seed.list[tt]
   set.seed(seeds)
 
-  trainIndex <- createDataPartition(all_points$DON, p = 0.85, list = FALSE)  
+  trainIndex <- createDataPartition(all_points$DON, p = 0.9, list = FALSE)  
   training <- all_points[trainIndex,]
   testing <- all_points[-trainIndex,]
   
@@ -487,7 +489,7 @@ for (tt in c(1:20)){
   
   M4_ACC_train<-postResample(map4_train$data$response,map4_train$data$truth)[1]
 
-  sing_acc<-data.frame(M1_ACC,M2_ACC,M4_ACC,M1_ACC_train,M2_ACC_train,M4_ACC_train,M1_kappa,M2_kappa,M4_kappa)
+  sing_acc<-data.frame(aaaa,M1_ACC,M2_ACC,M4_ACC,M1_ACC_train,M2_ACC_train,M4_ACC_train,M1_kappa,M2_kappa,M4_kappa)
   
   all_results<-rbind(all_results,sing_acc)
   print(a1)
@@ -496,3 +498,5 @@ for (tt in c(1:20)){
  
 }
 print(summary(all_results))
+
+}
